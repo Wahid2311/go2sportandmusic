0ffa132f08214a9d96de3440139fbe5fb39e1b97
Wahid2311 <119957770+Wahid2311@users.noreply.github.com>
2025-09-27 23:49:29 +0600

    Update models.py

--- tickets/models.py
+++ tickets/models.py
@@ -131,7 +131,7 @@ class Ticket(models.Model):
             section.upper_price = max(prices)
             section.save()
         if is_new:
-            event.total_tickets = sum(t.number_of_tickets for t in event.tickets.all())
+            self.event.total_tickets += self.number_of_tickets
             event.save()
 
         #self.update_event_section_aggregates()
