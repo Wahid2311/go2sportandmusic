a374d0d122b9abddfa66db138556b8810316deff
JavedAkhtar1220 <j.akhtar1220@gmail.com>
2026-01-14 18:22:29 +0500

    fix: country code for Antarctica

--- static/events/js/countryData.js
+++ static/events/js/countryData.js
@@ -15,7 +15,7 @@ window.COUNTRIES = [
     { code: 'AL', label: 'Albania', phone: '355' },
     { code: 'AM', label: 'Armenia', phone: '374' },
     { code: 'AO', label: 'Angola', phone: '244' },
-    { code: 'AQ', label: 'Antarctica', phone: '672' },
+    { code: 'AN', label: 'Antarctica', phone: '672' },
     { code: 'AR', label: 'Argentina', phone: '54' },
     { code: 'AS', label: 'American Samoa', phone: '1-684' },
     { code: 'AT', label: 'Austria', phone: '43' },
