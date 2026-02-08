07c4e8836344ddb68ac58b487dcf9aff4ae536c7
sabbir4512 <mesabbir4512@gmail.com>
2026-02-08 23:49:46 +0600

    Fix: Add migration files to resolve conflicts

--- events/migrations/0007_merge.py
+++ events/migrations/0007_merge.py
@@ -7,6 +7,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('events', '0006_fix_eventcategory_table'),
+        ('events', '0006_rename_eventcategory_fields'),
     ]
 
     operations = [
