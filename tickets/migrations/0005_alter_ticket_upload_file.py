f07b57363b9dd5944a9586a89305ef85e66d9cbe
Developer <dev@go2sportandmusic.com>
2026-02-12 11:50:22 -0500

    Fix: Rename migration to fix duplicate 0002 migration numbers

--- tickets/migrations/0005_alter_ticket_upload_file.py
+++ tickets/migrations/0005_alter_ticket_upload_file.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tickets', '0001_initial'),
+        ('tickets', '0004_stripe_fields'),
     ]
 
     operations = [
