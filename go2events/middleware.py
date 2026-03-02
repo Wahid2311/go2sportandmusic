844a848c9cfe70c4f04f2af7ddb6d8113f749198
sabbir4512 <64387044+sabbir4512@users.noreply.github.com>
2026-03-02 17:52:10 +0000

    CRITICAL FIX: Remove infinite redirect loop - tickethouse.net should NOT redirect to itself

--- go2events/middleware.py
+++ go2events/middleware.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponsePermanentRedirect
 from django.utils.deprecation import MiddlewareMixin
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class DomainRedirectMiddleware(MiddlewareMixin):
@@ -9,8 +12,8 @@ class DomainRedirectMiddleware(MiddlewareMixin):
         # Get the host from the request
         host = request.get_host().split(':')[0]  # Remove port if present
         
-        # Redirect old domain to new domain ONLY
-        if host == 'go2sportandmusic.com' or host == 'www.go2sportandmusic.com':
+        # ONLY redirect old domain to new domain
+        if host.lower() in ['go2sportandmusic.com', 'www.go2sportandmusic.com']:
             # Build the new URL with the new domain
             new_url = request.build_absolute_uri(request.get_full_path())
             new_url = new_url.replace('go2sportandmusic.com', 'tickethouse.net')
@@ -18,12 +21,15 @@ class DomainRedirectMiddleware(MiddlewareMixin):
             # Ensure HTTPS
             if not new_url.startswith('https://'):
                 new_url = new_url.replace('http://', 'https://')
+            logger.info(f"Redirecting {host} to {new_url}")
             return HttpResponsePermanentRedirect(new_url)
         
         # For tickethouse.net, only redirect HTTP to HTTPS
-        if host == 'tickethouse.net' and not request.is_secure():
+        if host.lower() == 'tickethouse.net' and not request.is_secure():
             new_url = request.build_absolute_uri(request.get_full_path())
             new_url = new_url.replace('http://', 'https://')
+            logger.info(f"Redirecting HTTP to HTTPS for {host}")
             return HttpResponsePermanentRedirect(new_url)
         
+        # For all other hosts (including tickethouse.net HTTPS), don't redirect
         return None
