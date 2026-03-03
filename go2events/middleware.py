fb636ef69621dc7cd75427b5533aeb11bba2aee5
sabbir4512 <64387044+sabbir4512@users.noreply.github.com>
2026-03-03 06:38:59 +0000

    CRITICAL FIX: Remove infinite redirect loop - tickethouse.net should NOT redirect to itself

--- go2events/middleware.py
+++ go2events/middleware.py
@@ -1,35 +1,25 @@
 from django.http import HttpResponsePermanentRedirect
-from django.utils.deprecation import MiddlewareMixin
-import logging
+from django.conf import settings
 
-logger = logging.getLogger(__name__)
-
-
-class DomainRedirectMiddleware(MiddlewareMixin):
-    """Middleware to redirect old domain to new domain and HTTP to HTTPS"""
+class DomainRedirectMiddleware:
+    """
+    Middleware to redirect old domain to new domain.
+    ONLY redirect go2sportandmusic.com to tickethouse.net
+    DO NOT redirect tickethouse.net to itself
+    """
     
-    def process_request(self, request):
-        # Get the host from the request
-        host = request.get_host().split(':')[0]  # Remove port if present
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        # Get the host header
+        host = request.META.get('HTTP_HOST', '').lower()
         
-        # ONLY redirect old domain to new domain
-        if host.lower() in ['go2sportandmusic.com', 'www.go2sportandmusic.com']:
-            # Build the new URL with the new domain
-            new_url = request.build_absolute_uri(request.get_full_path())
-            new_url = new_url.replace('go2sportandmusic.com', 'tickethouse.net')
-            new_url = new_url.replace('www.go2sportandmusic.com', 'tickethouse.net')
-            # Ensure HTTPS
-            if not new_url.startswith('https://'):
-                new_url = new_url.replace('http://', 'https://')
-            logger.info(f"Redirecting {host} to {new_url}")
-            return HttpResponsePermanentRedirect(new_url)
+        # ONLY redirect if it's the OLD domain
+        if host.startswith('go2sportandmusic.com'):
+            # Redirect to new domain
+            return HttpResponsePermanentRedirect(f"https://tickethouse.net{request.path}")
         
-        # For tickethouse.net, only redirect HTTP to HTTPS
-        if host.lower() == 'tickethouse.net' and not request.is_secure():
-            new_url = request.build_absolute_uri(request.get_full_path())
-            new_url = new_url.replace('http://', 'https://')
-            logger.info(f"Redirecting HTTP to HTTPS for {host}")
-            return HttpResponsePermanentRedirect(new_url)
-        
-        # For all other hosts (including tickethouse.net HTTPS), don't redirect
-        return None
+        # For all other domains (including tickethouse.net), just serve normally
+        response = self.get_response(request)
+        return response
