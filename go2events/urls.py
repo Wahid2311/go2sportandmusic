45adc09f5be8ac4e464b468db2d366a088aeb50c
sabbir4512 <64387044+sabbir4512@users.noreply.github.com>
2026-03-16 03:08:10 +0600

    Update urls.py
        # Add your bot API path directly to the master router:
        path('api/bot/receive-tickets/', receive_bot_data, name='receive_bot_data'),

--- go2events/urls.py
+++ go2events/urls.py
@@ -3,11 +3,14 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from tickets.views import receive_bot_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('events.urls')),
     path('accounts/',include('accounts.urls')),
+    # Add your bot API path directly to the master router:
+    path('api/bot/receive-tickets/', receive_bot_data, name='receive_bot_data'),
     # path('tickets/',include('tickets.urls')),
 ]
 
